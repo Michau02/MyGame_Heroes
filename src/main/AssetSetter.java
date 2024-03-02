e962cf30669d470944ed9c8feca886725bff033e
Michau02 <michal.kaluska@gmail.com>
2024-03-02 23:59:13 +0100

    zmiana sposobu dodawania obiektow w AssetSetter

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -15,25 +15,44 @@ public class AssetSetter {
     }
     public void setObject(){
         //OBJECTS
-        gp.obj.add(new OBJ_Wood());
-        gp.obj.get(0).worldX = 5*gp.tileSize;
-        gp.obj.get(0).worldY = 10*gp.tileSize;
+        set(0,5,10); // type - 0 -> wood
+                                // type = 1 -> gold
 
         gp.obj.add(new OBJ_Wood());
-        gp.obj.get(1).worldX = 9*gp.tileSize;
-        gp.obj.get(1).worldY = 10*gp.tileSize;
+        gp.obj.get(1).worldX = 16*gp.tileSize;
+        gp.obj.get(1).worldY = 12*gp.tileSize;
 
         gp.obj.add(new OBJ_Wood());
-        gp.obj.get(2).worldX = 5*gp.tileSize;
-        gp.obj.get(2).worldY = 15*gp.tileSize;
+        gp.obj.get(2).worldX = 9*gp.tileSize;
+        gp.obj.get(2).worldY = 17*gp.tileSize;
+
+        gp.obj.add(new OBJ_Wood());
+        gp.obj.get(3).worldX = 39*gp.tileSize;
+        gp.obj.get(3).worldY = 11*gp.tileSize;
+
+        gp.obj.add(new OBJ_Wood());
+        gp.obj.get(4).worldX = 26*gp.tileSize;
+        gp.obj.get(4).worldY = 21*gp.tileSize;
+
+        gp.obj.add(new OBJ_Wood());
+        gp.obj.get(5).worldX = 44*gp.tileSize;
+        gp.obj.get(5).worldY = 10*gp.tileSize;
 
         gp.obj.add(new OBJ_Gold());
-        gp.obj.get(3).worldX = 6*gp.tileSize;
-        gp.obj.get(3).worldY = 12*gp.tileSize;
+        gp.obj.get(6).worldX = 2*gp.tileSize;
+        gp.obj.get(6).worldY = 4*gp.tileSize;
 
         gp.obj.add(new OBJ_Gold());
-        gp.obj.get(4).worldX = 8*gp.tileSize;
-        gp.obj.get(4).worldY = 13*gp.tileSize;
+        gp.obj.get(7).worldX = 6*gp.tileSize;
+        gp.obj.get(7).worldY = 19*gp.tileSize;
+
+        gp.obj.add(new OBJ_Gold());
+        gp.obj.get(7).worldX = 6*gp.tileSize;
+        gp.obj.get(7).worldY = 19*gp.tileSize;
+
+        gp.obj.add(new OBJ_Gold());
+        gp.obj.get(7).worldX = 6*gp.tileSize;
+        gp.obj.get(7).worldY = 19*gp.tileSize;
 
         //CURRENT RESOURCES
         gp.objInv.add(new OBJ_WoodInv());
@@ -49,5 +68,19 @@ public class AssetSetter {
         gp.cas[0].worldX = 6*gp.tileSize;
         gp.cas[0].worldY = 8*gp.tileSize;
         gp.player.playersCastles.add(gp.cas[0]);
+    }
+    public void set(int type, int x, int y) {
+        if(type >= 0 && type < 2){
+            switch (type) {
+                case 0:
+                    gp.obj.add(new OBJ_Wood());
+                    break;
+                case 1:
+                    gp.obj.add(new OBJ_Gold());
+                    break;
+            }
+            gp.obj.get(gp.obj.size()-1).worldX = x*gp.tileSize;
+            gp.obj.get(gp.obj.size()-1).worldY = y*gp.tileSize;
+        }
     }
 }
